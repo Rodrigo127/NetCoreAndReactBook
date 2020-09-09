15f54638d626dd28bddeb3057401270ceca78c89
Rodrigo <rodrigo127001@gmail.com>
2020-09-09 08:27:26 -0500

    Added props to different components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { fontFamily, fontSize, gray2 } from './Styles';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
       <Header />
       <HomePage />
     </div>
--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { PrimaryButton } from './Styles';
+import { QuestionList } from './QuestionList';
+import { getUnansweredQuestions } from './QuestionsData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
 
 export const HomePage = () => (
-  <div>
-    <div>
-      <h2>Unanswered Questions</h2>
-      <button>Ask a question</button>
+  <Page>
+    <div
+      css={css`
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      `}
+    >
+      <PageTitle>Unanswered Questions</PageTitle>
+      <PrimaryButton>Ask a question</PrimaryButton>
     </div>
-  </div>
+    <QuestionList data={getUnansweredQuestions()} />
+  </Page>
 );
